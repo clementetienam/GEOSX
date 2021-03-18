3182efe353413edb256fd5cf8192a615076ac899
Brian Han <han12@llnl.gov>
2021-03-18 16:44:09 -0700

    Monolith geosx_core library

--- src/cmake/GeosxMacros.cmake
+++ src/cmake/GeosxMacros.cmake
@@ -54,3 +54,19 @@ macro(geosx_add_code_checks)
     endif()
 
 endmacro(geosx_add_code_checks)
+
+
+
+##------------------------------------------------------------------------------
+## convert_filenames_to_full_paths( NAMES file1 file 2... )
+##
+## A handy function to add the current source directory to a local
+## filename. To be used for creating a list of sources.
+##------------------------------------------------------------------------------
+function(convert_filenames_to_full_paths NAMES)
+  unset(tmp_names)
+  foreach(name ${${NAMES}})
+    list(APPEND tmp_names ${CMAKE_CURRENT_SOURCE_DIR}/${name})
+  endforeach()
+  set(${NAMES} ${tmp_names} PARENT_SCOPE)
+endfunction()
